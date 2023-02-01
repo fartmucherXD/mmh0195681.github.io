422b2559cf1fe94bbf54b991c273a433c92597db
Myla' Shay Harris <mmh0195681@lpssonline.com>
2023-02-01 14:50:06 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -19,14 +19,18 @@ var init = function (window) {
         ///////////////// PROGRAM SETUP ////////////////////////////
         ////////////////////////////////////////////////////////////
         
-        // TODO 1 : Declare and initialize our variables
-
+        // TODO 1 : Declare and initialize our variables //
+        var circle; 
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+        physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle);
+        circles.push(circle);
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        drawCircles(5);
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,10 +43,19 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            physikz.updatePosition( /* Your Bracket Notation HERE */ );
+ 	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
+ 	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
+ 	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
+ 	        physikz.updatePosition( /* Your Bracket Notation HERE */ );
+        }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
 
             // TODO 9 : Iterate over the array
            
